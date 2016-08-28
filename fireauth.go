fd9ef0d04618b0876dd92ba90b839f24ed6e1bd0
Kenneth Shaw <kenneth.shaw@knq.io>
2016-08-28 16:32:52 +0700

    Initial commit

---
+++ fireauth.go
@@ -0,0 +1,146 @@
+// Package fireauth provides a simple way to create auth tokens for use with firebase v3.0.0+ API.
+package fireauth
+
+import (
+	"encoding/json"
+	"errors"
+	"strconv"
+	"time"
+
+	"github.com/knq/jwt"
+)
+
+const (
+	claimAudience = "https://identitytoolkit.googleapis.com/google.identity.identitytoolkit.v1.IdentityToolkit"
+
+	// DefaultExpirationDuration is the default expiration duration.
+	DefaultExpirationDuration time.Duration = 2 * time.Hour
+)
+
+// Claims is the JWT payload claims for a Firebase access token.
+type Claims struct {
+	// Issuer ("iss") identifies the principal that issued the JWT.
+	Issuer string `json:"iss,omitempty"`
+
+	// Subject ("sub") identifies the principal that is the subject of the JWT.
+	Subject string `json:"sub,omitempty"`
+
+	// Audience ("aud") identifies the recipients that the JWT is intended for.
+	Audience string `json:"aud,omitempty"`
+
+	// IssuedAt ("iat") identifies the time at which the JWT was issued.
+	IssuedAt json.Number `json:"iat,omitempty"`
+
+	// NotBefore ("nbf") identifies the time before which the JWT MUST NOT be
+	// accepted for processing.
+	NotBefore json.Number `json:"nbf,omitempty"`
+
+	// Expiration ("exp") identifies the expiration time on or after which the
+	// JWT MUST NOT be accepted for processing.
+	Expiration json.Number `json:"exp,omitempty"`
+
+	// UserID identifies the authenticated user to Firebase.
+	//
+	// This is copied from the parent, but can be overridden with the UserID
+	// TokenOption.
+	UserID string `json:"uid,omitempty"`
+
+	// Claims is the additional authenticated user data for Firebase.
+	//
+	// This can be set with the AuthData TokenOption, and is then available in
+	// the Firebase `auth` and `request.auth` variables.
+	Claims interface{} `json:"claims,omitempty"`
+}
+
+// fireauth wraps a token signer with the required data.
+type fireauth struct {
+	signer jwt.Signer
+
+	projectID           string
+	serviceAccountEmail string
+
+	enableExpiration bool
+	enableIssuedAt   bool
+	enableNotBefore  bool
+
+	expirationDuration time.Duration
+}
+
+// New creates a new fireauth using the provided options.
+func New(opts ...Option) (*fireauth, error) {
+	var err error
+
+	// default options
+	f := &fireauth{
+		enableExpiration: true,
+		enableIssuedAt:   true,
+		enableNotBefore:  true,
+
+		expirationDuration: DefaultExpirationDuration,
+	}
+
+	// apply options
+	for _, o := range opts {
+		err = o(f)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// check that signer is defined
+	if f.signer == nil {
+		return nil, errors.New("no private key was provided")
+	}
+
+	// check issuer and subject
+	if f.serviceAccountEmail == "" {
+		return nil, errors.New("no service account email was provided")
+	}
+
+	return f, nil
+}
+
+// Token generates a token with the provided options.
+func (f *fireauth) Token(opts ...TokenOption) ([]byte, error) {
+	tok := &Claims{
+		Issuer:   f.serviceAccountEmail,
+		Subject:  f.serviceAccountEmail,
+		Audience: claimAudience,
+	}
+
+	now := time.Now()
+	n := json.Number(strconv.FormatInt(now.Unix(), 10))
+
+	// set expiration
+	if f.enableExpiration {
+		tok.Expiration = json.Number(strconv.FormatInt(now.Add(f.expirationDuration).Unix(), 10))
+	}
+
+	// set issued at
+	if f.enableIssuedAt {
+		tok.IssuedAt = n
+	}
+
+	//  set not before
+	if f.enableNotBefore {
+		tok.NotBefore = n
+	}
+
+	// apply options
+	for _, o := range opts {
+		o(tok)
+	}
+
+	// encode
+	return f.signer.Encode(tok)
+}
+
+// TokenString generates a token with the provided options.
+func (f *fireauth) TokenString(opts ...TokenOption) (string, error) {
+	buf, err := f.Token(opts...)
+	if err != nil {
+		return "", err
+	}
+
+	return string(buf), nil
+}
---
+++ options.go
@@ -0,0 +1,169 @@
+package fireauth
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"time"
+
+	"github.com/knq/jwt"
+)
+
+// Option represents a fireauth option.
+type Option func(*fireauth) error
+
+// ProjectID is an Option that sets the project id.
+func ProjectID(id string) Option {
+	return func(f *fireauth) error {
+		f.projectID = id
+		return nil
+	}
+}
+
+// ServiceAccountEmail is an Option that sets the service account email (ie,
+// the issuer and subject).
+func ServiceAccountEmail(email string) Option {
+	return func(f *fireauth) error {
+		f.serviceAccountEmail = email
+		return nil
+	}
+}
+
+// PEM is an Option that reads the key from pem.
+//
+// Note: please see knq/jwt.PEM for how to use this
+func PEM(pem jwt.PEM) Option {
+	return func(f *fireauth) error {
+		var err error
+		f.signer, err = jwt.RS256.New(pem)
+		return err
+	}
+}
+
+// CredentialsJSON is an Option that reads all the relevant settings from the
+// supplied JSON-encoded buf (ie, the contents of the JSON file downloaded from
+// the Firebase console).
+func CredentialsJSON(buf []byte) Option {
+	return func(f *fireauth) error {
+		var err error
+
+		// the members to extract from the json data.
+		vals := struct {
+			ProjectID   string `json:"project_id"`
+			ClientEmail string `json:"client_email"`
+			PrivateKey  string `json:"private_key"`
+		}{}
+
+		// read vals from json
+		err = json.Unmarshal(buf, &vals)
+		if err != nil {
+			return err
+		}
+
+		if vals.ProjectID == "" || vals.ClientEmail == "" || vals.PrivateKey == "" {
+			return errors.New("credentials missing project_id, client_email or private_key")
+		}
+
+		// set project id
+		err = ProjectID(vals.ProjectID)(f)
+		if err != nil {
+			return err
+		}
+
+		// set service account email
+		err = ServiceAccountEmail(vals.ClientEmail)(f)
+		if err != nil {
+			return err
+		}
+
+		// set private key
+		return PEM(jwt.PEM{[]byte(vals.PrivateKey)})(f)
+	}
+}
+
+// CredentialsJSONString is an Option that reads all the relevant settings from
+// the supplied JSON-encoded str (ie, the contents of the JSON file downloaded
+// from the Firebase console).
+func CredentialsJSONString(str string) Option {
+	return CredentialsJSON([]byte(str))
+}
+
+// CredentialsFile is a fireauth Option that reads all the relevant settings
+// from the supplied JSON-encoded file located at path.
+func CredentialsFile(path string) Option {
+	return func(f *fireauth) error {
+		buf, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		return CredentialsJSON(buf)(f)
+	}
+}
+
+// ExpirationDuration is an Option that sets the expiration duration.
+func ExpirationDuration(d time.Duration) Option {
+	return func(f *fireauth) error {
+		if d != 0 {
+			f.enableExpiration = true
+			f.expirationDuration = d
+		} else {
+			f.enableExpiration = false
+			f.expirationDuration = 0
+		}
+
+		return nil
+	}
+}
+
+// IssuedAt is an option that sets whether or not the issued at ("iat") field
+// is set on the token.
+func IssuedAt(enable bool) Option {
+	return func(f *fireauth) error {
+		f.enableIssuedAt = enable
+		return nil
+	}
+}
+
+// NotBefore is an option that sets whether or not the not before ("nbf") field
+// is set on the token.
+func NotBefore(enable bool) Option {
+	return func(f *fireauth) error {
+		f.enableNotBefore = enable
+		return nil
+	}
+}
+
+// AuthUserID is an option to set the authenticated user id ("uid").
+//
+// This will be copied on all subsequent calls to Token, and can be overridden
+// using the UserID TokenOption.
+func AuthUserID(uid string) Option {
+	return func(f *fireauth) error {
+		return nil
+	}
+}
+
+// TokenOption represents a fireauth claims option.
+type TokenOption func(*Claims)
+
+// UserID specifies (or overrides) the authenticated user id ("uid").
+//
+// See also: AuthUserID
+func UserID(uid string) TokenOption {
+	return func(c *Claims) {
+		c.UserID = uid
+	}
+}
+
+// AuthData sets the extra authenticated claims data to v.
+//
+// The data will be encoded into the token claims, and can be accessed via the
+// Firebase `auth` and `request.auth` security rules variables.
+//
+// Note: v must be json.Marshal-able.
+func AuthData(v interface{}) TokenOption {
+	return func(c *Claims) {
+		c.Claims = v
+	}
+}
