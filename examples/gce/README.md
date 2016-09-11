89a046200b0c819ae6a109a195f6ad7425efce40
Kenneth Shaw <kenneth.shaw@knq.io>
2016-09-11 07:47:15 +0700

    Updating gce example with documentation on scopes

---
+++ examples/gce/README.md
@@ -0,0 +1,24 @@
+# About gce
+
+This example shows how to use the firebase package with Google Compute Service
+credentials, using Google's standard [oauth2/google](https://godoc.org/golang.org/x/oauth2/google)
+auth token providers.
+
+## Projects and Scopes
+
+Please note that when using `google.ComputeTokenSource`, you must create
+an instance that has the appropriate scopes for Firebase's v3+ API, either by
+creating a custom service account credential in the [IAM console](https://console.cloud.google.com/iam-admin/serviceaccounts)
+or when creating the compute instance.
+
+The scopes needed for Firebase v3+ are:
+
+* https://www.googleapis.com/auth/userinfo.email
+* https://www.googleapis.com/auth/firebase.database
+
+Scopes can be passed via the `gcloud` cli tool using the `--scopes` option:
+
+```sh
+# this uses the short-hand notation available for the userinfo.email scope
+$ gcloud compute instances create <INSTANCE_NAME> --scopes userinfo-email,https://www.googleapis.com/auth/firebase.database
+```
--- examples/gce/gce.go
+++ examples/gce/gce.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"golang.org/x/oauth2"
@@ -14,12 +15,32 @@ func main() {
 	var err error
 
 	// use the Google compute credentials with an oauth2 transport
+	//
+	// NOTE: your compute instance needs to have been created using the
+	// apropriate firebase scopes for this to work!
+	//
+	// For example, using the gcloud tool:
+	//
+	// gcloud compute instances create my-test-project-1 --scopes userinfo-email,https://www.googleapis.com/auth/firebase.database
 	db, err := firebase.NewDatabaseRef(
+		firebase.URL("https://<PROJECT ID>.firebaseio.com/"),
 		firebase.Transport(&oauth2.Transport{
 			Source: google.ComputeTokenSource(""),
 		}),
 	)
-	log.Fatal(err)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// set some data
+	for i := 0; i < 5; i++ {
+		_, err = db.Ref("/people").Push(map[string]interface{}{
+			"name": fmt.Sprintf("person %d", i),
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	// retrieve the /people keys
 	keys := make(map[string]interface{})
@@ -28,4 +49,12 @@ func main() {
 		log.Fatal(err)
 	}
 	log.Printf("keys: %+v", keys)
+
+	// delete all the keys
+	for k, _ := range keys {
+		err = db.Ref("/people/" + k).Remove()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
