cc3822ccbb46fdf7a24d196241b029d89216d329
Kenneth Shaw <kenneth.shaw@knq.io>
2016-11-24 10:27:22 +0700

    Adding firebase-get cli util

---
+++ cmd/firebase-get/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/knq/firebase"
+)
+
+var (
+	flagCredentials = flag.String("creds", "", "path to google service account credentials")
+	flagRef         = flag.String("ref", "/", "firebase ref to retrieve")
+	flagVerbose     = flag.Bool("v", false, "verbose logging")
+)
+
+func main() {
+	var err error
+
+	flag.Parse()
+
+	// check credentials
+	if *flagCredentials == "" {
+		fmt.Fprintf(os.Stderr, "error: invalid credentials file\n")
+		os.Exit(1)
+	}
+
+	// build firebase options
+	opts := []firebase.Option{
+		firebase.GoogleServiceAccountCredentialsFile(*flagCredentials),
+	}
+	if *flagVerbose {
+		opts = append(opts, firebase.Log(log.Printf, log.Printf))
+	}
+
+	// create database ref
+	ref, err := firebase.NewDatabaseRef(opts...)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// retrieve ref
+	var v interface{}
+	err = ref.Ref(*flagRef).Get(&v)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// pretty format
+	buf, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+	log.Printf("received: %s", string(buf))
+}
